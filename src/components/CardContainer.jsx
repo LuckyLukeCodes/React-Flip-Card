e98c0dcd68b4fee7e654b1daa7e3186c68e94787
luckyluke <llcodes@outlook.com>
2024-03-31 15:08:57 -0400

    Added responsive styling

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -4,7 +4,10 @@ import CardBack from "./CardBack";
 
 const CardContainer = ({ text, image }) => {
   return (
-    <div className="card relative h-72 w-96 cursor-pointer overflow-hidden rounded-2xl text-white shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] transition-all duration-700">
+    <div
+      className="card relative h-72 w-96 shrink-0 cursor-pointer overflow-hidden 
+    rounded-2xl text-white shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] transition-all duration-700"
+    >
       <CardFront image={image} />
       <CardBack text={text} />
     </div>
